ed618f63f8dc596f44eaea3e965acc958dce7cdc
FrowningCat <vsavinkov60@gmail.com>
2023-12-03 13:38:25 +0300

    add scroll and new page

--- app/src/main/java/com/example/historyinpersonalities/MainActivity.kt
+++ app/src/main/java/com/example/historyinpersonalities/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.historyinpersonalities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var linkToCategory: TextView = findViewById(R.id.linkToCat)
+
+        linkToCategory.setOnClickListener {
+            val intent = Intent(this, article::class.java)
+            startActivity(intent)
+        }
     }
 }
