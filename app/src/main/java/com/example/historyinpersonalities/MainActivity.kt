7d836ce29d5e3a6bb0fa3f7b1d5ce2b9117c5756
FrowningCat <vsavinkov60@gmail.com>
2023-12-03 15:38:28 +0300

    added new pages and link

--- app/src/main/java/com/example/historyinpersonalities/MainActivity.kt
+++ app/src/main/java/com/example/historyinpersonalities/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.historyinpersonalities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -11,9 +12,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var linkToCategory: TextView = findViewById(R.id.linkToCat)
+        var linkToPeten: TextView = findViewById(R.id.linkToPeten)
+        var linkToError: ImageView = findViewById(R.id.linkToError)
 
         linkToCategory.setOnClickListener {
             val intent = Intent(this, article::class.java)
+            startActivity(intent)
+        }
+        linkToError.setOnClickListener {
+            val intent = Intent(this, error::class.java)
+            startActivity(intent)
+        }
+        linkToPeten.setOnClickListener {
+            val intent = Intent(this, peten::class.java)
             startActivity(intent)
         }
     }
